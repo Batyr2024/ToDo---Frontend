89559b71a06c8568d10db1d814a6cfabd4bc5494
Batyr Abdusalamov <ugffnmtqoek6dbadoe4kleq1gknb5w@bots.bitbucket.org>
2024-03-05 15:43:52 +0300

    Fix bugs

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 (() => {
-  let arrayTask = [];
-  const { _ } = window;
   const taskInput = document.getElementById('taskInput');
   const addTaskButton = document.getElementById('addTask');
   const taskList = document.getElementById('taskList');
-  const taskDeleted = document.getElementById('taskDeleted');
+  const taskSelectedDeleted = document.getElementById('taskSelectedDeleted');
   const checkboxSelectAll = document.getElementById('checkboxSelectAll');
-  let idGlobalArray = 1;
-  const actual = { actual: 'all', selectPage: 1, maxPageActive: 1 };
   const spanAll = document.getElementById('spanAll');
   const spanActive = document.getElementById('spanActive');
   const spanCompleted = document.getElementById('spanCompleted');
@@ -15,27 +11,49 @@
   const pActive = document.getElementById('pActive');
   const pCompleted = document.getElementById('pCompleted');
   const paginationDiv = document.getElementById('pagination');
+  let maxPageAll = 1;
+  let maxPageActive = 1;
+  let maxPageComplete = 1;
+  let selectPagination = 1;
+  let actualTab = 'all';
   const keyEnter = 'Enter';
   const keyEscape = 'Escape';
   const maxTaskPage = 5;
+  let idGlobalArray = 1;
+  let arrayTask = [];
+  const { _ } = window;
 
   function addPage(lengthArray) {
-    let length = 0;
-    if (actual.actual === 'all') {
-      length = arrayTask.length;
-    } else {
-      length = lengthArray;
-    }
-    const page = Math.ceil(length / maxTaskPage);
+    const countPages = Math.ceil(lengthArray / maxTaskPage);
     let listHtml = '';
-    for (let i = 0; page > i; i += 1) {
-      listHtml += `
-      <a href="#" class=page id=${String(i + 1)}>${String(i + 1)}</a>
-      `;
+    for (let i = 0; countPages > i; i += 1) {
+      switch (actualTab) {
+        case 'all':
+          if (maxPageAll < selectPagination) {
+            listHtml += `<a href="#" class=${maxPageAll === i + 1 ? 'active-page' : 'page'} id=${String(i + 1)}>${String(i + 1)}</a>`;
+          } else {
+            listHtml += `<a href="#" class=${selectPagination === i + 1 ? 'active-page' : 'page'} id=${String(i + 1)}>${String(i + 1)}</a>`;
+          }
+          break;
+        case 'active':
+          if (maxPageActive < selectPagination) {
+            listHtml += `<a href="#" class=${maxPageActive === i + 1 ? 'active-page' : 'page'} id=${String(i + 1)}>${String(i + 1)}</a>`;
+          } else {
+            listHtml += `<a href="#" class=${selectPagination === i + 1 ? 'active-page' : 'page'} id=${String(i + 1)}>${String(i + 1)}</a>`;
+          }
+          break;
+        case 'complete':
+          if (maxPageComplete < selectPagination) {
+            listHtml += `<a href="#" class=${maxPageComplete === i + 1 ? 'active-page' : 'page'} id=${String(i + 1)}>${String(i + 1)}</a>`;
+          } else {
+            listHtml += `<a href="#" class=${selectPagination === i + 1 ? 'active-page' : 'page'} id=${String(i + 1)}>${String(i + 1)}</a>`;
+          }
+          break;
+        default: break;
+      }
     }
     paginationDiv.innerHTML = '';
     paginationDiv.innerHTML += listHtml;
-    document.getElementById(actual.selectPage).style = 'background:#74a5e0;';
   }
   function counter() {
     spanAll.textContent = '0';
@@ -47,14 +65,59 @@
     spanCompleted.textContent = String(arrayCompleted.length);
     spanAll.textContent = String((arrayActive.length) + (arrayCompleted.length));
   }
+  function counterPage() {
+    let arrayTaskPage;
+    maxPageAll = Math.ceil(arrayTask.length / maxTaskPage);
+    arrayTaskPage = arrayTask.filter((value) => value.isChecked === false);
+    maxPageActive = Math.ceil(arrayTaskPage.length / maxTaskPage);
+    arrayTaskPage = arrayTask.filter((value) => value.isChecked === true);
+    maxPageComplete = Math.ceil(arrayTaskPage.length / maxTaskPage);
+    switch (actualTab) {
+      case 'all':
+        if (selectPagination > maxPageAll) {
+          selectPagination = maxPageAll;
+        }
+        break;
+      case 'active':
+        if (selectPagination > maxPageActive) {
+          selectPagination = maxPageActive;
+        }
+        break;
+      case 'complete':
+        if (selectPagination > maxPageComplete) {
+          selectPagination = maxPageComplete;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
   function render() {
-    actual.actual = 'all';
-    taskList.innerHTML = ' ';
-    let listHtml = '';
-    addPage();
-    const endIndex = actual.selectPage * maxTaskPage;
+    const endIndex = selectPagination * maxTaskPage;
     const startIndex = endIndex - maxTaskPage;
-    const arrayTaskPage = arrayTask.slice(startIndex, endIndex);
+    let lengthArray;
+    let arrayTaskPage;
+    switch (actualTab) {
+      case 'all':
+        arrayTaskPage = arrayTask.slice(startIndex, endIndex);
+        lengthArray = arrayTask.length;
+        break;
+      case 'active':
+        arrayTaskPage = arrayTask.filter((value) => value.isChecked === false);
+        lengthArray = arrayTaskPage.length;
+        arrayTaskPage = arrayTaskPage.slice(startIndex, endIndex);
+        break;
+      case 'complete':
+        arrayTaskPage = arrayTask.filter((value) => value.isChecked === true);
+        lengthArray = arrayTaskPage.length;
+        arrayTaskPage = arrayTaskPage.slice(startIndex, endIndex);
+        break;
+      default:
+        break;
+    }
+    let listHtml = '';
+    taskList.innerHTML = ' ';
     arrayTaskPage.forEach((element) => {
       const idCheckbox = element.id;
       const taskText = element.text;
@@ -65,80 +128,31 @@
   <span id=${idCheckbox} class="delete">X</span>
   </li>
       `;
-      counter();
     });
     taskList.innerHTML += listHtml;
-    actual.selectPage = 1;
-  }
-  function showActive() {
-    actual.actual = 'active';
-    let listHtml = '';
-    let arrayTaskPage = arrayTask.filter((value) => value.isChecked === false);
-    const lengthArray = arrayTaskPage.length;
+    counter();
+    counterPage();
     addPage(lengthArray);
-    const endIndex = actual.selectPage * maxTaskPage;
-    const startIndex = endIndex - maxTaskPage;
-    arrayTaskPage = arrayTaskPage.slice(startIndex, endIndex);
-    arrayTaskPage.forEach((element) => {
-      const idCheckbox = element.id;
-      const taskText = element.text;
-      if (element.isChecked === false) {
-        listHtml += `
-  <li>
-  <input class="checkbox" id=${idCheckbox} type ="checkbox">
-  <span id="${idCheckbox}" >${_.escape(taskText)}</span>
-  <span id=${idCheckbox} class="delete">X</span>
-  </li>
-      `;
-      }
-    });
-    taskList.innerHTML = '';
-    taskList.innerHTML += listHtml;
-    actual.maxPageActive = Math.ceil(lengthArray / maxTaskPage);
-    actual.selectPage = 1;
   }
-
-  function showCompleted() {
-    actual.actual = 'complete';
-    let listHtml = '';
-    let arrayTaskPage = arrayTask.filter((value) => value.isChecked === true);
-    const lengthArray = arrayTaskPage.length;
-    addPage(lengthArray);
-    const endIndex = actual.selectPage * maxTaskPage;
-    const startIndex = endIndex - maxTaskPage;
-    arrayTaskPage = arrayTaskPage.slice(startIndex, endIndex);
-    arrayTaskPage.forEach((element) => {
-      const idCheckbox = element.id;
-      const taskText = element.text;
-      if (element.isChecked === true) {
-        listHtml += `
-  <li>
-  <input class="checkbox" checked  id=${idCheckbox} type ="checkbox">
-  <span id='${idCheckbox}' >${_.escape(taskText)}</span>
-  <span id=${idCheckbox} class="delete">X</span>
-  </li>
-      `;
-      }
-    });
-    taskList.innerHTML = '';
-    taskList.innerHTML += listHtml;
-    actual.selectPage = 1;
+  function tabAll() {
+    actualTab = 'all';
+    selectPagination = maxPageAll;
+    render();
+  }
+  function tabActive() {
+    actualTab = 'active';
+    selectPagination = maxPageActive;
+    render();
+  }
+  function tabCompleted() {
+    actualTab = 'complete';
+    selectPagination = maxPageComplete;
+    render();
   }
   function selectPage(event) {
-    document.getElementById(actual.selectPage).style = 'background:#74a5e0;';
     if (event.target.className === 'page') {
-      actual.selectPage = Number(event.target.id);
-      switch (actual.actual) {
-        case 'active':
-          showActive();
-          break;
-        case 'complete':
-          showCompleted();
-          break;
-        default:
-          render();
-          break;
-      }
+      selectPagination = Number(event.target.id);
+      render();
     }
   }
   function handleAddTask() {
@@ -149,47 +163,26 @@
       text: taskText,
       isChecked: false,
     });
-    switch (actual.actual) {
+    switch (actualTab) {
       case 'all':
-        actual.selectPage = Math.ceil(arrayTask.length / maxTaskPage);
-        render();
+        selectPagination = Math.ceil(arrayTask.length / maxTaskPage);
         break;
       case 'active':
-        actual.selectPage = actual.maxPageActive;
-        showActive();
+        selectPagination = maxPageActive;
         break;
       default:
         break;
     }
+    render();
     counter();
     taskInput.value = '';
   }
   function selectAllCheckboxTasks(event) {
-    switch (actual.actual) {
-      case 'all':
-        arrayTask.forEach((element) => {
-          const arrayElement = element;
-          arrayElement.isChecked = event.target.checked;
-        });
-        render();
-        break;
-      case 'active':
-        arrayTask.forEach((element) => {
-          const arrayElement = element;
-          arrayElement.isChecked = event.target.checked;
-        });
-        showActive();
-        break;
-      case 'complete':
-        arrayTask.forEach((element) => {
-          const arrayElement = element;
-          arrayElement.isChecked = event.target.checked;
-        });
-        showCompleted();
-        break;
-      default:
-        break;
-    }
+    arrayTask.forEach((element) => {
+      const arrayElement = element;
+      arrayElement.isChecked = event.target.checked;
+    });
+    render();
     counter();
   }
   function checkboxClicksTasks(event) {
@@ -199,11 +192,9 @@
       let checkedAllTrue = false;
       if (arrayTask[index].isChecked === false) {
         arrayTask[index].isChecked = true;
-        if (actual.actual === 'active') { showActive(); }
       } else {
         arrayTask[index].isChecked = false;
-        if (actual.actual === 'complete') { showCompleted(); }
-      }
+      } render();
       for (let i = 0; i < arrayTask.length; i += 1) {
         if (arrayTask[i].isChecked === false) {
           checkedAllTrue = false; break;
@@ -221,40 +212,24 @@
     if (elementClass === 'delete') {
       const index = arrayTask.findIndex((item) => item.id === Number(elementId));
       arrayTask.splice(index, 1);
-      switch (actual.actual) {
-        case 'active':
-          showActive();
-          break;
-        case 'complete':
-          showCompleted();
-          break;
-        default:
-          render();
-          break;
-      }
     }
+    counterPage();
+    render();
     counter();
   }
   function deleteSelectTasks() {
     arrayTask = arrayTask.filter((value) => value.isChecked === false);
-    switch (actual.actual) {
-      case 'active':
-        showActive();
-        break;
-      case 'complete':
-        showCompleted();
-        break;
-      default:
-        render();
-        break;
-    }
+    counterPage();
+    render();
     checkboxSelectAll.checked = false;
     counter();
   }
   function changeTasks(event) {
     const itemId = Number(event.target.id.split('-')[1]);
     const spanElement = document.querySelector(`#taskText-${itemId}`);
+    console.log(itemId);
     arrayTask.forEach((element) => {
+      console.log(element.id);
       if (element.id === itemId) {
         idGlobalArray = itemId;
         const listHtml = document.createElement('input');
@@ -285,15 +260,15 @@
 
   addTaskButton.addEventListener('click', handleAddTask);
   checkboxSelectAll.addEventListener('click', selectAllCheckboxTasks);
-  taskList.addEventListener('change', checkboxClicksTasks);
+  taskList.addEventListener('click', checkboxClicksTasks);
   taskList.addEventListener('click', deleteThisTask);
-  taskDeleted.addEventListener('click', deleteSelectTasks);
+  taskSelectedDeleted.addEventListener('click', deleteSelectTasks);
   taskInput.addEventListener('keyup', addTaskPressKey);
   taskList.addEventListener('dblclick', changeTasks);
   taskList.addEventListener('keyup', addTaskPressKey);
   taskList.addEventListener('blur', addChangeTasks, true);
-  pAll.addEventListener('click', render);
-  pActive.addEventListener('click', showActive);
-  pCompleted.addEventListener('click', showCompleted);
+  pAll.addEventListener('click', tabAll);
+  pActive.addEventListener('click', tabActive);
+  pCompleted.addEventListener('click', tabCompleted);
   paginationDiv.addEventListener('click', selectPage);
 })();
